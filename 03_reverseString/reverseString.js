ea375742d0b07a2e70147edeb07f5a66be9cdd89
Miikka Kettunen <miikka.kettunen@gmail.com>
2022-04-04 00:41:17 +0300

    task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(reverseInput) {
+    let reverseString = "";
+        for(let i = reverseInput.length; i >= 0; i--){
+            reverseString = reverseString + reverseInput.charAt(i);
+        }
+    return reverseString;
 };
 
 // Do not edit below this line
