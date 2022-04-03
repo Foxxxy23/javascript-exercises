363cb5dedcea5ebf97b1a006703e2fbe1d300601
Miikka Kettunen <miikka.kettunen@gmail.com>
2022-04-04 00:19:28 +0300

    2 tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    if(amount < 0){
+        let repeatString = "ERROR"
+        return repeatString;
+    }
+    let repeatString = ""
+    for (let a = 0; a < amount; a++) {
+        repeatString = repeatString + word;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
