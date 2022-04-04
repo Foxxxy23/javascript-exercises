09b92fa7487ef6e819417f637b75c8c3b651be4f
Miikka Kettunen <miikka.kettunen@gmail.com>
2022-04-04 03:17:46 +0300

    task 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray, ...numberRemoval) {
+    console.log("Array before mods = " + userArray)
+    let counter = 1;
+        for(let i = 0; i < numberRemoval.length; i++){
+            if(typeof numberRemoval[i] === "number"){
+                let removedItem = userArray.splice((numberRemoval[i]-counter), 1);
+                console.log("Index of removed item = " + (numberRemoval[i]-counter));
+                console.log("Name of removed item = " + removedItem);
+                console.log("Array after mods = " + userArray);
+                console.log("counter = " + counter);
+                counter = counter + 1;
+            }
+            if(typeof numberRemoval[i] === "string" && userArray.includes(numberRemoval[i])){
+                userArray.splice(numberRemoval[i]-counter, 1);
+                console.log(userArray);
+                console.log("Value of removed item = " + (numberRemoval[i]-1));
+                counter = counter + 1;
+            }
+        }
+    let removeFromArray = userArray;
+    return removeFromArray;
 };
 
 // Do not edit below this line
