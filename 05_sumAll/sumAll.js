0e62b5e4e51b51fb497de87f9d67da9f8fb7abe6
Miikka Kettunen <miikka.kettunen@gmail.com>
2022-04-04 23:22:13 +0300

    task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let c = 0;
+    if(a < 0 || typeof a != "number"){
+        return "ERROR";
+    }
+    if(b < 0 || typeof b != "number"){
+        return "ERROR";
+    }
+    if(a < b){
+        while(a <= b){
+            c = c + a;
+            a = a + 1;
+            }
+        if(a > b){
+            let sumAll = c;
+            return sumAll;
+            }
 
+        }
+    if(a > b){
+        while(b <= a){
+            c = c + b;
+            b = b + 1;
+            }
+    }
+    let sumAll = c;
+    return sumAll;
 };
 
 // Do not edit below this line
