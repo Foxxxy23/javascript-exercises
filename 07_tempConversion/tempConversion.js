04b8b1ae14d07999744886d63b8402c3acc6071c
Miikka Kettunen <miikka.kettunen@gmail.com>
2022-04-04 23:56:53 +0300

    task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let ftoc = (far - 32) * (5/9);
+  ftoc = Math.round(ftoc * 10) / 10
+  console.log(ftoc);
+  return ftoc;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let ctof = cel*(9/5) + 32;
+  ctof = Math.round(ctof * 10) / 10
+  console.log(ctof);
+  return ctof;
 };
 
 // Do not edit below this line
