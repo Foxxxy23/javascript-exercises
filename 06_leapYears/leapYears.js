bd4a970f5f1ee154b53e713abb8a5da1f7170181
Miikka Kettunen <miikka.kettunen@gmail.com>
2022-04-04 23:45:50 +0300

    task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapYears = false;
+    if(year % 4 === 0){
+        if(year % 100 === 0 && year % 400 === 0){
+            leapYears = true;
+            return leapYears;
+        }
+        if(year % 100 === 0){
+            leapYears = false; 
+        }
+        else{
+            leapYears = true;
+        }
+    }
+    return leapYears;
 };
 
 // Do not edit below this line
